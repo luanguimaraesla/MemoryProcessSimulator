85d54175984cb6abbcfaee09812e93e81de71980
Luan Guimarães <luan@live.in>
2015-04-18 17:26:56 -0300

    Criando funções de inicialização das estruturas.

--- MPS.c
+++ MPS.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE_OF_MEMORY 200
+#define SIZE_OF_ALL_SPACES 200
 #define HOLE 0
 #define PROCESS 1
 
@@ -43,8 +43,10 @@ struct Process{
 };
 
 struct Memory{
-	struct MemoryCase *init;
-	struct MemoryCase *final;
+	struct MemoryCase *begin;
+	struct MemoryCase *end;
+	struct MemoryCase *firstHole;
+	struct MemoryCase *lastHole;
 };
 
 typedef struct MemoryCase MemoryCase;
@@ -54,9 +56,21 @@ typedef struct Memory Memory;
 
 /*--------------------FUNCTIONS HEADERS-----------------------*/
 
+Memory * initMemory(void);
+
+MemoryCase * makeInitialMemoryCase(void);
+
+Process *initProcess(space begin, numberOfSpaces inUse);
+
+Hole *makeHole(space begin, numberOfSpaces available, Hole *prevHole, Hole *nextHole);
+Hole *makeInitialHole(void);
+
 /*---------------------------MAIN-----------------------------*/
 
 int main(void){
+	Memory *memory;
+
+	memory = initMemory();
 
 	return 0;
 }
@@ -65,8 +79,58 @@ int main(void){
 
 /*---------------------MEMORY FUNCTIONS-----------------------*/
 
+Memory * initMemory(void){
+	Memory *newMemory;
+	MemoryCase *newMemoryCase;	
+
+	newMemory = (Memory *) malloc (sizeof(Memory));
+	newMemoryCase = makeInitialMemoryCase();
+
+	newMemory->begin = newMemoryCase;
+	newMemory->end = newMemoryCase;
+	newMemory->firstHole = newMemoryCase;
+	newMemory->lastHole = newMemoryCase;
+
+	return newMemory;
+}
+
 /*-------------------MEMORY CASE FUNCTIONS--------------------*/
+
+MemoryCase * makeInitialMemoryCase(void){
+	MemoryCase *newMemoryCase;
+	newMemoryCase = (MemoryCase *) malloc (sizeof(MemoryCase));
+	newMemoryCase->type = hole;
+	newMemoryCase->holeOrProcess = (void *) makeInitialHole();
+	newMemoryCase->next = NULL;
+	newMemoryCase->prev = NULL;
+
+	return newMemoryCase;
+}
 
 /*---------------------PROCESS FUNCTIONS----------------------*/
 
+Process *initProcess(space begin, numberOfSpaces inUse){
+	Process * newProcess;
+	newProcess = (Process *) malloc (sizeof(Process));
+	newProcess->begin = begin;
+	newProcess->inUse = inUse;
+
+	return newProcess;
+}
+
 /*-----------------------HOLE FUNCTIONS-----------------------*/
+
+Hole *makeHole(space begin, numberOfSpaces available,Hole *prevHole, Hole *nextHole){
+	Hole * newHole;
+	newHole = (Hole *) malloc (sizeof(Hole));
+	newHole->begin = begin;
+	newHole->available = available;
+	newHole->prevHole = prevHole;
+	newHole->nextHole = nextHole;
+
+	return newHole;
+}
+
+Hole *makeInitialHole(void){
+	return makeHole(0, SIZE_OF_ALL_SPACES, NULL, NULL);
+}
