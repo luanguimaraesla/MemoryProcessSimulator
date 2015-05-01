b9fe4c3da8cc7458b9afc99a3e58dfd56220d892
Luan Guimarães <luan@live.in>
2015-05-01 17:11:21 -0300

    Definindo estruturas do programa MMS.c.

--- MMS.c
+++ MMS.c
@@ -7,6 +7,8 @@
 
 typedef unsigned long space;
 typedef unsigned long numberOfSpaces;
+typedef unsigned long processID;
+typedef unsigned long numberOfProcesses;
 
 /*--------------------------ENUMS--------------------------*/
 
@@ -14,23 +16,42 @@ enum memoryElementType{
 	hole, process
 };
 
+typedef enum memoryElementType memoryElementType;
+
 /*-----------------------STRUCTS---------------------------*/
 
 struct MemoryCase{
-	
+	memoryElementType type;	
+	void *holeOrProcess;
+	space begin;
+	numberOfSpaces size;
+	struct MemoryCase *next;
+	struct MemoryCase *prev;
 };
 
 struct Hole{
-	
+	struct MemoryCase *nextHoleCase;
+	struct MemoryCase *prevHoleCase;
 };
 
 struct Process{
-
+	processID id;
+	struct MemoryCase *nextProcessCase;
+	struct MemoryCase *prevProcessCase;
 };
 
 struct Memory{
-
+	numberOfProcesses total;
+	numberOfProcesses running;
+	numberOfSpaces available;
+	numberOfSpaces inUse;
+	struct MemoryCase *begin;
 };
+
+typedef struct MemoryCase MemoryCase;
+typedef struct Process Process;
+typedef struct Hole Hole;
+typedef struct Memory Memory;
 
 /*-------------------------MAIN----------------------------*/
 
