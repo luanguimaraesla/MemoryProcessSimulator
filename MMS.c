e34726ad53ba0ed215363e32c40c61ebf0bfd31e
Luan Guimarães <luan@live.in>
2015-05-01 16:55:34 -0300

    Criando estrutura básica do novo gerenciador de memória utilizando threads em C.

---
+++ MMS.c
@@ -0,0 +1,44 @@
+/* Memory Manager Simulator */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*------------------------TYPEDEFS-------------------------*/
+
+typedef unsigned long space;
+typedef unsigned long numberOfSpaces;
+
+/*--------------------------ENUMS--------------------------*/
+
+enum memoryElementType{
+	hole, process
+};
+
+/*-----------------------STRUCTS---------------------------*/
+
+struct MemoryCase{
+	
+};
+
+struct Hole{
+	
+};
+
+struct Process{
+
+};
+
+struct Memory{
+
+};
+
+/*-------------------------MAIN----------------------------*/
+
+int main(void){
+	
+	return 0;
+}
+
+/*----------------------FUNCTIONS--------------------------*/
+
+
