d8ccbc2a72e9e83e99b87e0d27828714de8c1026
Luan Guimarães <luan@live.in>
2015-05-07 00:42:38 -0300

    Adicionando estruturas dos argumentos de cada thread criada.

---
+++ include/mms_thread_args.h
@@ -0,0 +1,30 @@
+#ifndef MMS_THREAD_ARGS
+#define MMS_THREAD_ARGS
+#include "mms_structures.h"
+#include "mms_gui_generation.h"
+
+struct ExecProcessArg{
+	struct Memory *memory;
+	struct MemoryCase *selfCase;
+};
+
+struct RandomCreateProcessesArg{
+	struct MemoryCase * (*addProcessFunction)(numberOfSpaces, priority, struct Memory *);
+	unsigned int numberOfProcesses;
+	numberOfSpaces maxProcessSize;
+	priority maxPriorityIndex;
+	unsigned int maxProcessGenerateSleep;
+	struct Memory *memory;
+};
+
+struct FrameUpdateArg{
+	bool * frame_update;
+	struct Memory *memory;
+	struct UIParameters *ui_params;
+};
+
+typedef struct ExecProcessArg execution_arg;
+typedef struct RandomCreateProcessesArg rcp_arg;
+typedef struct FrameUpdateArg fu_arg;
+
+#endif
